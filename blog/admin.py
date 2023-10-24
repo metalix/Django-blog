37da114b80cfe255a1d40c714b157ad378b8e817
rafalkozlowski <r.kozlowski@mac.com>
2023-10-24 21:26:44 +0200

    Created django application and customized article model

---
+++ blog/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Article
+
+# Register your models here.
+#admin.site.register(Article)
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_filter = ['status', 'created', 'publish', 'author']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
